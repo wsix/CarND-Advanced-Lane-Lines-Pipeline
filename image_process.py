9d46dbf2d22ae535381dcdc24bef50c11ec2e999
wsix <wsix0114@gmail.com>
2017-03-23 21:55:01 +0800

    Add the pipeline to process single images and some of its outputs.

---
+++ image_process.py
@@ -0,0 +1,110 @@
+import cv2
+import numpy as np
+
+
+def cal_undistort(img, objpoints, imgpoints):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    undist = cv2.undistort(img, mtx, dist, None, mtx)
+    return undist, mtx, dist
+
+
+def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
+    if orient == 'x':
+        sobel = cv2.Sobel(img, cv2.CV_64F, 1, 0)
+    else:
+        sobel = cv2.Sobel(img, cv2.CV_64F, 0, 1)
+    abs_sobel = np.absolute(sobel)
+    scaled_sobel = np.uint8(255 * abs_sobel / np.max(abs_sobel))
+
+    binary_output = np.zeros_like(scaled_sobel)
+    binary_output[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
+    return binary_output
+
+
+# Function to create a thresholded binary image
+def pipeline(img, s_thresh=(170, 255), v_thresh=(230, 255), x_thresh=(20, 100), sx_thresh=(20, 100)):
+    img = np.copy(img)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY).astype(np.float)
+    # Convert to HSV color space and separate the V channel
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV).astype(np.float)
+    v_channel = hsv[:, :, 2]
+    # Convert to HLS color space and separate the S channel
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS).astype(np.float)
+    s_channel = hls[:, :, 2]
+
+    v_binary = np.zeros_like(v_channel)
+    v_binary[(v_channel >= v_thresh[0]) & (v_channel <= v_thresh[1])] = 1
+    s_binary = np.zeros_like(s_channel)
+    s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+
+    ksize = 7
+    gradx = abs_sobel_thresh(gray, orient='x', sobel_kernel=ksize, thresh=x_thresh)
+    gradx_s = abs_sobel_thresh(v_channel, orient='x', sobel_kernel=ksize, thresh=sx_thresh)
+
+    color_binary = np.zeros_like(s_channel)
+    color_binary[(s_binary == 1) | (gradx == 1) | (gradx_s == 1) | (v_binary == 1)] = 1
+    return color_binary
+
+
+def fit_polynomial(binary_warped, nonzero):
+    histogram = np.sum(binary_warped[binary_warped.shape[0] / 2:, :], axis=0)
+
+    midpoint = np.int(histogram.shape[0] / 2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+    # Choose the number of sliding windows
+    nwindows = 9
+    # Set height of windows
+    window_height = np.int(binary_warped.shape[0] / nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated for each window
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    # Set the width of the windows +/- margin
+    margin = 100
+    # Set minimum number of pixels found to recenter window
+    minpix = 50
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+
+    # Step through the windows one by one
+    for window in range(nwindows):
+        win_y_low = binary_warped.shape[0] - (window + 1) * window_height
+        win_y_high = binary_warped.shape[0] - window * window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+    return left_fit, right_fit, left_lane_inds, right_lane_inds
+
